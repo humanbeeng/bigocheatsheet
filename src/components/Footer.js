0fc5d7bbd765291afc59fdd7b5ecfc203649b697
Nithin SJ <thenithinraj@gmail.com>
2021-03-01 16:54:21 +0530

    Fixed Footer issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import emoji from "react-easy-emoji";
 
 const Footer = () => {
-  return <div align="center">Made by humanbeeng ❤️</div>;
+  return <div className="footer">Made by humanbeeng ❤️</div>;
 };
 
 export default Footer;
